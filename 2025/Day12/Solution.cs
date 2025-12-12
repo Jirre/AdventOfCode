31ff69130e3aa6a47ca632333a2e848a6718f32a
Jirrev <jirreamon@gmail.com>
2025-12-12 08:41:21 +0100

    [+] Day 12

---
+++ 2025/Day12/Solution.cs
@@ -0,0 +1,64 @@
+namespace AdventOfCode.Y2025.Day12;
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+[ProblemName("Christmas Tree Farm")]
+class Solution : Solver {
+
+    private record Shape(int W, int H, int[] Counts);
+    
+    public object PartOne(string input) {
+        string[] cells = input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+
+        int[] areas = ParseAreas(cells[..^1]);
+        Shape[] regions = ParseShapes(cells[^1]);
+
+        int count = regions.Count(region => Fits(region, areas));
+
+        return count;
+    }
+
+    private static int[] ParseAreas(string[] shapes) {
+        int[] areas = new int[shapes.Length];
+
+        for (int i = 0; i < shapes.Length; i++) {
+            int area = 0;
+            foreach (char ch in shapes[i]) {
+                if (ch == '#') area++;
+            }
+            areas[i] = area;
+        }
+
+        return areas;
+    }
+
+    private static Shape[] ParseShapes(string block) {
+        string[] lines = block.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        Shape[] shapes = new Shape[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++) {
+            int[] numbers = Regex.Matches(lines[i], @"\d+")
+                .Select(m => int.Parse(m.Value))
+                .ToArray();
+
+            int w = numbers[0];
+            int h = numbers[1];
+            int[] counts = numbers[2..];
+
+            shapes[i] = new Shape(w, h, counts);
+        }
+
+        return shapes;
+    }
+
+    private static bool Fits(Shape shape, int[] areas) {
+        int needed = 0;
+
+        for (int i = 0; i < shape.Counts.Length; i++)
+            needed += shape.Counts[i] * areas[i];
+
+        return needed <= shape.W * shape.H;
+    }
+}
